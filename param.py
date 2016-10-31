aa6e4cbddbd7f0d05bb0b612ffd9945d5dd598db
ByungHo Lee <mercy76@gmail.com>
2016-11-01 08:38:27 +0900

    keyword param

---
+++ param.py
@@ -0,0 +1,10 @@
+def print_param2(**kwargs):
+    print (kwargs)
+    print (kwargs.keys())
+    print (kwargs.values())
+ 
+    for name, value in kwargs.items():
+        print ("%s : %s" % (name, value))
+
+print_param2(first = 'a', second = 'b', third = 'c', fourth = 'd')
+#print_param2(first = 'a', second = 'b', third = 'c', fourth = 'd')
