2ac3acd728430af21d40b0071375d9b73e0c2526
ByungHo Lee <mercy76@gmail.com>
2016-11-14 14:10:27 +0900

    change lint rule

--- hook.py
+++ hook.py
@@ -1,3 +1,3 @@
-names = ['Socrates', 'Archimedas', 'Plato', 'Aristotle' ]
+names = ['Socrates', 'Archimedas', 'Plato', 'Aristotle']
 names.sort(key=lambda x: len(x))
 print(names)
--- param.py
+++ param.py
@@ -3,9 +3,7 @@ def print_param2(**kwargs):
     print(kwargs)
     print(kwargs.keys())
     print(kwargs.values())
- 
     for name, value in kwargs.items():
         print("%s : %s" % (name, value))
 
 print_param2(first='a', second='b', third='c', fourth='d')
-#print_param2(first = 'a', second = 'b', third = 'c', fourth = 'd')
