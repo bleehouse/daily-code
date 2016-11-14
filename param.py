ac56102b807f4839e395e8755aca77a4298ecd1f
ByungHo Lee <mercy76@gmail.com>
2016-11-14 13:39:30 +0900

    change pylint

--- param.py
+++ param.py
@@ -1,10 +1,11 @@
+
 def print_param2(**kwargs):
-    print (kwargs)
-    print (kwargs.keys())
-    print (kwargs.values())
+    print(kwargs)
+    print(kwargs.keys())
+    print(kwargs.values())
  
     for name, value in kwargs.items():
-        print ("%s : %s" % (name, value))
+        print("%s : %s" % (name, value))
 
-print_param2(first = 'a', second = 'b', third = 'c', fourth = 'd')
+print_param2(first='a', second='b', third='c', fourth='d')
 #print_param2(first = 'a', second = 'b', third = 'c', fourth = 'd')
