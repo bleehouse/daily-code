ceb169029a66c1c5c2dcd287f4ced69ae8424b9c
ByungHo Lee <ByungHo Lee>
2016-12-19 23:08:08 +0900

    innerPerson.js

---
+++ innerPerson.js
@@ -0,0 +1,27 @@
+var Person = (function(){
+
+	var age = 25;
+	function InnerPerson(name) {
+		this.name = name;
+	}
+	
+	InnerPerson.prototype.getAge = function () {
+		return age;
+	};
+	
+	InnerPerson.prototype.growOlder = function () {
+		age++;
+	};	
+	
+	return InnerPerson;
+}());
+
+
+var person1 = new Person("Nicholas");
+var person2 = new Person("Greg");
+
+console.log(person1.name);
+console.log(person1.getAge());
+
+console.log(person2.name);
+console.log(person2.getAge());
