c2d9951b0d3b3a50a76014ad73ece80fd3ce43fb
ByungHo Lee <blee03291@gmail.com>
2016-12-27 15:43:10 +0900

    read big file

--- python/wordcount.py
+++ python/wordcount.py
@@ -1,14 +1,20 @@
-# import collections
+
+def readInChunks(fileObj, chunkSize=2048):
+    while True:
+        data = fileObj.read(chunkSize)
+        if not data:
+            break
+        yield data
 
 file = open("wordlist.txt", "r+")
 wordcount = {}
-for word in file.read().split():
-    if word not in wordcount:
-        wordcount[word] = 1
-    else:
-        wordcount[word] += 1
 
-# wordcount = collections.OrderedDict(sorted(wordcount.items()))
+for wordlist in readInChunks(file):
+    for word in wordlist.split():
+        if word not in wordcount:
+            wordcount[word] = 1
+        else:
+            wordcount[word] += 1
 
 for k, v in sorted(wordcount.items()):
     print(k + ":" + v * "*")
@@ -16,3 +22,4 @@ for k, v in sorted(wordcount.items()):
 
 # http://stackoverflow.com/questions/9001509/how-can-i-sort-a-dictionary-by-key
 # http://stackoverflow.com/questions/21107505/python-word-count-from-a-txt-file-program
+# http://stackoverflow.com/questions/8009882/how-to-read-large-file-line-by-line-in-python
