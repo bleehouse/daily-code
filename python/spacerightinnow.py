af33e6544827dda519372fc55d8bb0df2b7951b0
ByungHo Lee <blee03291@gmail.com>
2016-12-28 14:03:17 +0900

    add sorted function

--- python/spacerightinnow.py
+++ python/spacerightinnow.py
@@ -1,6 +1,9 @@
 import requests
 
-# https://pyformat.info/
+
+def sort_list(keyname, oldlist):
+    newlist = sorted(oldlist, key=lambda k: k[keyname])
+    return newlist
 
 r = requests.get('http://api.open-notify.org/astros.json')
 peoples = r.json()
@@ -8,10 +11,10 @@ peoples = r.json()
 print('There are {0} people in space right now:'.format(int(peoples["number"])) + '\r\n')
 print('{:20}'.format('Name') + ' | ' + '{:20}'.format('Craft'))
 print('{:20}'.format('_'*21) + '|' + '{:20}'.format('_'*21))
-for people in peoples["people"]:
+
+for people in sort_list("name", peoples["people"]):
     print('{:20}'.format(people["name"]) + ' | ' + '{:20}'.format(people["craft"]))
 
-
-
+# https://pyformat.info/
 
 
