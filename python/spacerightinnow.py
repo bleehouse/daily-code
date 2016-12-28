46ae299b0e66f40ec7aa17c63e4327478fe53c46
ByungHo Lee <blee03291@gmail.com>
2016-12-28 13:26:56 +0900

    cahange format

--- python/spacerightinnow.py
+++ python/spacerightinnow.py
@@ -7,7 +7,7 @@ peoples = r.json()
 
 print('There are {0} people in space right now:'.format(int(peoples["number"])) + '\r\n')
 print('{:20}'.format('Name') + ' | ' + '{:20}'.format('Craft'))
-print('{:20}'.format('____________________') + ' | ' + '{:20}'.format('___________________'))
+print('{:20}'.format('_'*21) + '|' + '{:20}'.format('_'*21))
 for people in peoples["people"]:
     print('{:20}'.format(people["name"]) + ' | ' + '{:20}'.format(people["craft"]))
 
