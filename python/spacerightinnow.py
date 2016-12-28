f1bbc6b74e194f4ffabccdc808721cd80c0d5acf
ByungHo Lee <blee03291@gmail.com>
2016-12-28 13:21:52 +0900

    init space astro

---
+++ python/spacerightinnow.py
@@ -0,0 +1,17 @@
+import requests
+
+# https://pyformat.info/
+
+r = requests.get('http://api.open-notify.org/astros.json')
+peoples = r.json()
+
+print('There are {0} people in space right now:'.format(int(peoples["number"])) + '\r\n')
+print('{:20}'.format('Name') + ' | ' + '{:20}'.format('Craft'))
+print('{:20}'.format('____________________') + ' | ' + '{:20}'.format('___________________'))
+for people in peoples["people"]:
+    print('{:20}'.format(people["name"]) + ' | ' + '{:20}'.format(people["craft"]))
+
+
+
+
+
