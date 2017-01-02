9ead50cc55a23510aed7e0f1eeb9d31717eff3fb
ByungHo Lee <blee03291@gmail.com>
2017-01-03 00:27:05 +0900

    do children's node

--- python/webscraping/getHtml2.py
+++ python/webscraping/getHtml2.py
@@ -4,7 +4,9 @@ html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
 bsobj = BeautifulSoup(html, "html.parser")
 # nameList = bsobj.findAll("span", {"class" : "green"})
 # nameList = bsobj.findAll({"h1", "h2", "h3", "h4", "h5", "h6"})
-nameList = bsobj.findAll("span", {"class": {"green", "red"}})
+# nameList = bsobj.findAll("span", {"class": {"green", "red"}})
+nameList = bsobj.findAll(text="the prince")
+# print(len(nameList))
 
 for name in nameList:
     print(name.get_text())
