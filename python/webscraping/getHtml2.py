71037cc9505a4a206344fed4aefab7e82bf45ca5
ByungHo Lee <blee03291@gmail.com>
2017-01-02 23:38:15 +0900

    test findAll using another parameter

--- python/webscraping/getHtml2.py
+++ python/webscraping/getHtml2.py
@@ -2,7 +2,8 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
 bsobj = BeautifulSoup(html, "html.parser")
-nameList = bsobj.findAll("span", {"class" : "green"})
+# nameList = bsobj.findAll("span", {"class" : "green"})
+nameList = bsobj.findAll({"h1", "h2", "h3", "h4", "h5", "h6"})
 
 for name in nameList:
     print(name.get_text())
