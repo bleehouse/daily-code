9e425fd6d20eebc1fbf85d7f6ac22b47dc30fc40
ByungHo Lee <blee03291@gmail.com>
2017-01-07 00:04:28 +0900

    add example

--- python/Fluent_Python/chapter.01/FrenchDeck.py
+++ python/Fluent_Python/chapter.01/FrenchDeck.py
@@ -1,6 +1,8 @@
 # 전문가를 위한 파이썬(Fluent Python) example 1-1 일련의 카드로 구성한 카드 한벌
 
 import collections
+from random import choice
+
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
@@ -18,6 +20,9 @@ class FrenchDeck:
     def __getitem__(self, position):
         return self._cards[position]
 
+    def __reversed__(self):
+        return reversed(self._cards)
+
 
 beer_card = Card('7', 'diamonds')
 
@@ -26,6 +31,15 @@ print(beer_card)
 deck = FrenchDeck()
 print(deck.ranks)
 print(deck.suits)
-print(deck.getCards())
 print(len(deck))
 print(deck[10])
+print(deck._cards)
+print(choice(deck))
+print(deck[:3])
+
+for card in deck: # doctest: +ELIPSIS
+    print(card)
+
+for card in reversed(deck):  # doctest: +ELIPSIS
+    print('------------') # 특별 메소드를 변경해 볼 수 있다.
+    print(card)
