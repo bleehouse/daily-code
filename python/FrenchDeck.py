732f89cdb2831dcec23e21b5318e6cf9ce564e38
ByungHo Lee <blee03291@gmail.com>
2017-01-07 00:18:56 +0900

    update java q&a

---
+++ python/FrenchDeck.py
@@ -0,0 +1,34 @@
+# 전문가를 위한 파이썬(Fluent Python) example 1-1 일련의 카드로 구성한 카드 한벌
+
+import collections
+from random import choice
+
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+beer_card = Card('7', 'diamonds')
+
+print(beer_card)
+
+deck = FrenchDeck()
+print(deck.ranks)
+print(deck.suits)
+print(deck.getCards())
+print(len(deck))
+print(deck[10])
+print(choice(deck))
