e27b51a6b2f4d748f729afa247de15415fa49357
ByungHo Lee <blee03291@gmail.com>
2017-01-08 23:13:14 +0900

    mpdify bug and add example

--- python/Fluent_Python/chapter.01/Vector.py
+++ python/Fluent_Python/chapter.01/Vector.py
@@ -12,7 +12,7 @@ class Vector:
     def __repr__(self):
         return 'Vertor(%r, %r)' % (self.x, self.y)
 
-    def __abs(self):
+    def __abs__(self):
         return hypot(self.x, self.y)
 
     def __bool__(self):
@@ -30,3 +30,7 @@ class Vector:
 v1 = Vector(2, 4)
 v2 = Vector(2, 1)
 print(v1 + v2)
+
+v = Vector(3, 4)
+print(abs(v))
+print(v * 3)
