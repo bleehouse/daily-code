45b94fd1a3d6bbdb111e5bb3d98e6fab40222f5d
ByungHo Lee <blee03291@gmail.com>
2017-01-12 22:51:45 +0900

    update list_example.2-5

--- python/Fluent_Python/chapter.02/list_example.2-5.py
+++ python/Fluent_Python/chapter.02/list_example.2-5.py
@@ -5,3 +5,9 @@ symbols = '%^*#@%'
 print(tuple(ord(symbol) for symbol in symbols))
 print(array.array('I', (ord(symbol) for symbol in symbols)))
 
+
+colors = ['black', 'white']
+sizes = ['S', 'M', 'L']
+
+for tshirt in ('%s %s' % (c, s) for c in colors for s in sizes):
+    print(*tshirt)
