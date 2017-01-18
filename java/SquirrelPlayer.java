c9c18fbbbc61cd0641410d0d40bfa2cc41e74491
ByungHo Lee <blee03291@gmail.com>
2017-01-18 23:05:07 +0900

    / CodingBat Logic-1 > squirrelPlay

---
+++ java/SquirrelPlayer.java
@@ -0,0 +1,26 @@
+// CodingBat Logic-1 > squirrelPlay 
+
+public class SquirrelPlayer {
+
+	public static boolean squirrelPlay(int temp, boolean isSummer) {
+		  int upperLimit = 90;
+		  boolean isSquirrelsPlay = false;
+		  if (isSummer == true) {
+		    upperLimit = 100;
+		  }
+		  
+		  if (temp >= 60 && temp <= upperLimit) {
+		    isSquirrelsPlay = true;
+		  }
+		  
+		  return isSquirrelsPlay;
+		}	
+	
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println(squirrelPlay(70, false));
+		System.out.println(squirrelPlay(95, false));
+		System.out.println(squirrelPlay(95, true));
+	}
+
+}
