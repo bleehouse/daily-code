6030ced1e5e4ab6e505ac24230990e0b938c97db
ByungHo Lee <blee03291@gmail.com>
2017-01-21 00:01:09 +0900

    specialEleven

--- java/specialEleven.java
+++ java/specialEleven.java
@@ -1,11 +1,10 @@
 public class specialEleven {
-	
-	public static boolean specialEleven(int n) {
+	public static boolean getSpecialEleven(int n) {
 		 int specialEleven = n % 11;
 		 return specialEleven == 0 ||specialEleven == 1;  
 	}	
 	
 	public static void main(String[] args) {
-		System.out.println(specialEleven(22));
+    	System.out.println(getSpecialEleven(22));
 	}
 }
