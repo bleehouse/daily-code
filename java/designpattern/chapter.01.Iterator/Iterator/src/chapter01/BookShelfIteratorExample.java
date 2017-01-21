a9af995de82bb1986dfa49e5f08c269765313be5
ByungHo Lee <blee03291@gmail.com>
2017-01-21 23:57:31 +0900

    init Iterator Pattern

---
+++ java/designpattern/chapter.01.Iterator/Iterator/src/chapter01/BookShelfIteratorExample.java
@@ -0,0 +1,24 @@
+package chapter01;
+
+public class BookShelfIteratorExample {
+
+	public static void main(String[] args) {
+
+		BookShelf bookshelf = new BookShelf(4);
+		
+		bookshelf.appendBook(new Book("Around the World in 80 Days"));
+		bookshelf.appendBook(new Book("Bible"));
+		bookshelf.appendBook(new Book("Cinderella"));
+		bookshelf.appendBook(new Book("Daddy-Long-Legs"));
+		
+		Iterator it = (Iterator) bookshelf.iterator();
+		
+		while(it.hasNext()) {
+			Book book = (Book)it.next();
+			System.out.println(book.getName());
+		}
+		
+	}
+
+}
+
