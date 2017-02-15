703df8ea530cbc2416d7fd6273f172059dc79234
ByungHo Lee <blee03291@gmail.com>
2017-02-15 23:29:15 +0900

    updata member.java using lombok

--- java/JpaExample/src/main/java/com/example/entity/Member.java
+++ java/JpaExample/src/main/java/com/example/entity/Member.java
@@ -6,7 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
 @Entity
+@lombok.Getter
+@lombok.Setter
+@lombok.ToString()
 public class Member {
 
 	@Id
@@ -25,33 +29,4 @@ public class Member {
 		this.name = name;
 		this.age = age;
 	}
- 
-	public long getId() {
-		return id;
-	}
- 
-	public void setId(long id) {
-		this.id = id;
-	}
- 
-	public String getName() {
-		return name;
-	}
- 
-	public void setName(String name) {
-		this.name = name;
-	}
- 
-	public int getAge() {
-		return age;
-	}
- 
-	public void setAge(int age) {
-		this.age = age;
-	}
-	
-	@Override
-	public String toString() {
-		return "[" + id + "] name = " + name + ", age = " + age;
-	}	
 }
