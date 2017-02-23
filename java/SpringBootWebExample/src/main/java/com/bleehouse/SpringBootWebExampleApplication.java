31521c23f4df2bb2a8a1028ae41e7f6687fd5180
ByungHo Lee <blee03291@gmail.com>
2017-02-23 23:43:33 +0900

    add converter

--- java/SpringBootWebExample/src/main/java/com/bleehouse/SpringBootWebExampleApplication.java
+++ java/SpringBootWebExample/src/main/java/com/bleehouse/SpringBootWebExampleApplication.java
@@ -3,13 +3,17 @@ package com.bleehouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import com.bleehouse.converter.LocalDateTimeConverter;
 
 import com.bleehouse.filter.HelloFilter;
 import com.bleehouse.interceptor.HttpInterceptor;
 
 @SpringBootApplication
+@ComponentScan
 public class SpringBootWebExampleApplication {
 
 	public static void main(String[] args) {
@@ -28,6 +32,12 @@ public class SpringBootWebExampleApplication {
             public void addInterceptors(InterceptorRegistry registry) {
                 registry.addInterceptor(new HttpInterceptor());
             }
+            
+            @Override
+            public void addFormatters(FormatterRegistry registry) {
+//                registry.addConverter(new LocalDateConverter("yyyy-MM-dd"));
+                registry.addConverter(new LocalDateTimeConverter("yyyy-MM-dd'T'HH:mm:ss.SSS"));
+            }            
         };
     }
 
