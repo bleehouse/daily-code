1e95626a024831428fb566b592005a1dd2182954
ByungHo Lee <blee03291@gmail.com>
2017-03-14 22:24:04 +0900

    add cpuusage.py

---
+++ python/cpuusage.py
@@ -0,0 +1,8 @@
+import psutil
+print(psutil.cpu_times())
+
+for x in range(3):
+    print(psutil.cpu_percent(interval=1))
+
+for x in range(3):
+    print(psutil.cpu_percent(interval=1, percpu=True))
