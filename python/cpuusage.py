0d0704bd30b93c240358f556a20a674ac0a5720a
ByungHo Lee <blee03291@gmail.com>
2017-03-14 22:31:09 +0900

    update cpuusage.py

--- python/cpuusage.py
+++ python/cpuusage.py
@@ -6,3 +6,14 @@ for x in range(3):
 
 for x in range(3):
     print(psutil.cpu_percent(interval=1, percpu=True))
+
+for x in range(3):
+    print(psutil.cpu_times_percent(interval=1, percpu=False))
+
+print(psutil.cpu_count())
+
+print(psutil.cpu_count(logical=False))
+
+print(psutil.cpu_stats())
+
+print(psutil.cpu_freq())
